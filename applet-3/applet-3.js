8dc37a9566deb3283495074c516d91a1db421b0e
lowPrio12 <ninar.ranin@gmail.com>
2024-10-10 23:23:34 +0800

    added displayLogCount function, the purpose of this displays total log counts

--- applet-3/applet-3.js
+++ applet-3/applet-3.js
@@ -35,4 +35,8 @@ class DataLogger {
     countLogs() {
         return this.loggedData.length;
     }
+    displayLogCount() {
+        const logCount = this.countLogs();
+        this.logCountElement.innerHTML = `<p>Total Logs: ${logCount}</p>`; 
+    }
 }
