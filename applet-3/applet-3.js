826e704eaffd1a2196f88fadb6c1bf5b1bde9a73
lowPrio12 <ninar.ranin@gmail.com>
2024-10-10 23:32:51 +0800

    Added Instantiate DataLogger on DOMContentLoaded

--- applet-3/applet-3.js
+++ applet-3/applet-3.js
@@ -40,3 +40,8 @@ class DataLogger {
         this.logCountElement.innerHTML = `<p>Total Logs: ${logCount}</p>`; 
     }
 }
+    document.addEventListener('DOMContentLoaded', () => {
+        new DataLogger('logButton', 'cardContainer', 'clearButton', 'logCount'); 
+    });
+    
+
