24f4f99a8e611670d64e4faadcbd0b0d00c39c6e
lowPrio12 <ninar.ranin@gmail.com>
2024-10-16 23:55:19 +0800

    Added fetchData method to retrieve the student's data

--- applet-4/applet-4.js
+++ applet-4/applet-4.js
@@ -10,5 +10,12 @@ class StudentList {
         this.renderStudentList(this.students); 
         this.bindSearchEvent();
     }
-
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
+            this.students = await response.json();
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    } this
 }
