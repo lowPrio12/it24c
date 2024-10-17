6a8876a0bc05893925db279edc87295a4c95b28a
lowPrio12 <ninar.ranin@gmail.com>
2024-10-18 00:47:31 +0800

    Added error handling for fetch operation

--- gallery.js
+++ gallery.js
@@ -1,8 +1,9 @@
 let applets = [];
 
 fetch('data.json')
-.then(response => response.json())
-.then(data => {
-  applets = data;
-  renderApplets(applets); 
+    .then(response => response.json())
+    .then(data => {
+        applets = data;
+        renderApplets(applets); 
+  .catch (error => console.error('Error fetching JSON:', error));
 
