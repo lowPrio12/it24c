9b3a60c7a32fc61c575cf863bfb9eb6dba44bece
lowPrio12 <ninar.ranin@gmail.com>
2024-11-01 13:04:50 +0800

    added weather description display reference in WeatherApp class

--- applet-5/applet-5.js
+++ applet-5/applet-5.js
@@ -14,5 +14,6 @@ class WeatherApp {
         this.weatherCard = document.getElementById('weatherCard');
         this.cityName = document.getElementById('cityName');
         this.temperature = document.getElementById('temperature');
+        this.description = document.getElementById('description');
     }
 }
