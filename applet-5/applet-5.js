0c5ef2b1cc1b6601159758b1daa76c4bdb9a5656
lowPrio12 <ninar.ranin@gmail.com>
2024-11-01 13:10:57 +0800

    modefied and added event listeners for weather retrieval buttons in WeatherApp class

--- applet-5/applet-5.js
+++ applet-5/applet-5.js
@@ -5,10 +5,10 @@ class WeatherApp {
 
          //Text Input
          this.cityInput = document.getElementById('cityInput');
-         this.getWeatherBtn = document.getElementById('WeatherBtn');
+         this.WeatherBtn = document.getElementById('WeatherBtn');
 
          //Geolocation Input
-        this.getLocationBtn = document.getElementById('LocationBtn');
+        this.LocationBtn = document.getElementById('LocationBtn');
         
         //Weather Card
         this.weatherCard = document.getElementById('weatherCard');
@@ -17,5 +17,9 @@ class WeatherApp {
         this.description = document.getElementById('description');
         this.humidity = document.getElementById('humidity');
         this.windSpeed = document.getElementById('windSpeed');
+
+         //Event Listener
+         this.WeatherBtn.addEventListener('click', () => this.fetchWeather());
+         this.LocationBtn.addEventListener('click', () => this.fetchWeatherByLocation());
     }
 }
